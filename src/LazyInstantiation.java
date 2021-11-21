6dbc6c7bf19a3385fd8d67fd25a8937fd851318a
Taranjit Singh <taranjits532@gmail.com>
2021-11-21 22:58:17 +0000

    method for eturning unique instance

--- src/LazyInstantiation.java
+++ src/LazyInstantiation.java
@@ -2,6 +2,19 @@ public class LazyInstantiation {
     private static LazyInstantiation Taoiseach = null ;
     private int number = 0;
     private LazyInstantiation() {}
+    public static LazyInstantiation getTaoiseach()
+    {
+        if (Taoiseach == null)
+            Taoiseach = new LazyInstantiation();
+
+        return Taoiseach;
+    }
+    public void setnumber(int n){
+        this.number = n;
+    }
+    public int getNumber(){
+        return number;
+    }
 
 
 
