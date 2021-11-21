e4a18058f41717446bb331824b94a17a200eaec1
Taranjit Singh <taranjits532@gmail.com>
2021-11-21 22:34:31 +0000

    declare wrong number of Taoiseach

--- src/Taoiseach_Check.java
+++ src/Taoiseach_Check.java
@@ -7,6 +7,14 @@ public class Taoiseach_Check{
 
         System.out.println(t1.hashCode());
         System.out.println(t2.hashCode());
+        if (t1==t2){
+            t1.setNumber_of_Taoiseach(1);
+            System.out.println("There can be only " + t1.getNumber_of_Taoiseach() + " Taoiseach in ireland");
+        }
+        else{
+            t2.setNumber_of_Taoiseach(2);
+            System.out.println("There are " + t2.getNumber_of_Taoiseach() + " Taoiseach in Ireland");
+        }
 
 
     }
