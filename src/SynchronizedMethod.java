368ba857c37d25f71d6514007c8e91c966c79f2f
Taranjit Singh <taranjits532@gmail.com>
2021-11-22 22:40:24 +0000

    Synchronized keyword every thread to wait its turn

--- src/SynchronizedMethod.java
+++ src/SynchronizedMethod.java
@@ -2,7 +2,7 @@ public class SynchronizedMethod {
     private static  SynchronizedMethod Taoiseach = null ;
     private int number = 0;
     private SynchronizedMethod() {}
-    public static SynchronizedMethod getTaoiseach()
+    public static synchronized SynchronizedMethod getTaoiseach()
     {
         if (Taoiseach == null)
             Taoiseach = new SynchronizedMethod();
