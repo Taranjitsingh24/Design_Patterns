03a6f329c87105e6835551a1977830e811e73357
Taranjit Singh <taranjits532@gmail.com>
2021-11-22 23:18:54 +0000

    synchronized in if statement to check first time

--- src/DoubleCheckedLocking.java
+++ src/DoubleCheckedLocking.java
@@ -1,4 +1,19 @@
 public class DoubleCheckedLocking {
-    private static volatile DoubleCheckedLocking  Taoiseach = null ;
+    private static volatile DoubleCheckedLocking Taoiseach = null;
+    private int number = 0;
 
+    private DoubleCheckedLocking() {
+    }
+
+    public static synchronized DoubleCheckedLocking getTaoiseach() {
+        if (Taoiseach == null){
+            synchronized (DoubleCheckedLocking.class) {
+                if (Taoiseach == null){
+                    Taoiseach = new DoubleCheckedLocking();
+                }
+            }
+        }
+        return Taoiseach;
+
+    }
 }
