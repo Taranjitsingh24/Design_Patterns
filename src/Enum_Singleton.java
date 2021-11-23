4f6f75df09d351b728e577556719aca9e510d76d
Taranjit Singh <taranjits532@gmail.com>
2021-11-23 16:21:08 +0000

    creating variable

--- src/Enum_Singleton.java
+++ src/Enum_Singleton.java
@@ -1,4 +1,14 @@
 public enum Enum_Singleton {
-    Taoiseach
+    Taoiseach;
+    int number = 0;
+    public void setnumber(int n){
+        this.number = n;
+    }
+    public int getNumber(){
+        return number;
+    }
 
 }
+
+
+
