5b6a6d0e4516ae6455025fd848aeaa4dab126fff
Taranjit Singh <taranjits532@gmail.com>
2021-12-09 15:17:06 +0000

    Protected Component

--- src/Decorator_pattern/ClothingDecorator.java
+++ src/Decorator_pattern/ClothingDecorator.java
@@ -2,4 +2,12 @@ package Decorator_pattern;
 
 public abstract class ClothingDecorator implements Clothing{
 
+protected Clothing decoratedStyle;
+public  ClothingDecorator (Clothing decoratedStyle){
+    this.decoratedStyle = decoratedStyle;
+
+
+}
+
+
 }
