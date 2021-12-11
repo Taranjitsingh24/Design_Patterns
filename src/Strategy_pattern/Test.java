d3442268dd87381f28a5c79520323b128a750384
Taranjit Singh <taranjits532@gmail.com>
2021-12-11 22:16:20 +0000

    algorithms are interchangeable

--- src/Strategy_pattern/Test.java
+++ src/Strategy_pattern/Test.java
@@ -3,8 +3,11 @@ package Strategy_pattern;
 public class Test {
     public static void main(String[] args) {
       Bowler spinbowler = new SpinBowler();
+      Bowler fastbowler = new FastBowler();
       IrishCricketTeam player1 = new Player1(spinbowler);
       player1.display();
+//      player1.bowl();
+      player1.setBowler(fastbowler);
       player1.bowl();
     }
 }
