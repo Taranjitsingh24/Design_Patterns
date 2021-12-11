75308aca40ac3aed4fff4a59b15f69c4c1307b3a
Taranjit Singh <taranjits532@gmail.com>
2021-12-11 22:13:09 +0000

    concrete class for Bowler

---
+++ src/Strategy_pattern/FastBowler.java
@@ -0,0 +1,10 @@
+package Strategy_pattern;
+
+public class FastBowler implements Bowler{
+
+    @Override
+    public void bowl() {
+        System.out.println("Role in team: fast bowler");
+
+    }
+}
