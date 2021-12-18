e46b3a727319f4d83fb4ad14014ae208ea57ed02
Taranjit Singh <taranjits532@gmail.com>
2021-12-18 17:35:40 +0000

    class implemented using factory pattern

---
+++ src/Factory_pattern/LaptopFactory.java
@@ -0,0 +1,16 @@
+package Factory_pattern;
+
+public class LaptopFactory {
+    public static Laptop buildLaptop(LaptopType model){
+            Laptop laptop = null;
+            switch(model){
+                case WINDOW:
+                    laptop = new WindowType();
+                    break;
+                default:
+                    break;
+            }
+            return laptop;
+    }
+
+}
